3b783b17730a47abb8c860b442fec2c81b3ac72e
Mohamed Khan <mkhan377@gmail.com>
2026-03-08 11:10:14 +0800

    feat: add market order rjection when no liquidity

--- apps/matching-engine/internal/engine/matcher.go
+++ apps/matching-engine/internal/engine/matcher.go
@@ -1,6 +1,6 @@
 package engine
 import (
-	"fmt"
+	"errors"
 	"time"
 	"github.com/khantopa/opentrade/matching-engine/internal/models"
 )
@@ -39,16 +39,22 @@ func (m *Matcher) Match(order models.Order) ([]Trade, error) {
 
 	// Market Order Rejection
 	if order.Type == models.OrderTypeMarket {
+
+		errorMessage := ""
+
 		if order.Side == models.OrderSideBuy && len(books.Asks) == 0 {
 			order.Status = models.OrderStatusRejected
-			books.Orders[order.ID] = order
-			return nil, fmt.Errorf("market buy order rejected: no asks available for ticker %s", order.Ticker)
-		}
-
+			errorMessage = "market buy order rejected: no asks available for ticker " + order.Ticker
+		}  
+		
 		if order.Side == models.OrderSideSell && len(books.Bids) == 0 {
 			order.Status = models.OrderStatusRejected
-			books.Orders[order.ID] = order
-			return nil, fmt.Errorf("market sell order rejected: no bids available for ticker %s", order.Ticker)
+			errorMessage = "market sell order rejected: no bids available for ticker " + order.Ticker
+		}
+
+		if errorMessage != "" {
+		  books.Orders[order.ID] = order
+			return nil, errors.New(errorMessage)
 		}
 	}
 
